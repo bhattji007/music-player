a1265ebf666c0c03685eda6d02160a957f091006
bhattji007 <53906268+bhattji007@users.noreply.github.com>
2021-05-22 14:02:32 +0530

    Add files via upload

---
+++ hello/index.js
@@ -0,0 +1,47 @@
+window.addEventListener("load", () => {
+  const sounds = document.querySelectorAll(".sound");
+  const pads = document.querySelectorAll(".pads div");
+  const visual = document.querySelector(".visual");
+  const mainMenu = document.querySelector('.mainMenu');
+  const closeMenu = document.querySelector('.closeMenu');
+  const openMenu = document.querySelector('.openMenu');
+
+  const colors = [
+    "#60d394",
+    "#d36060",
+    "#c060d3",
+    "#d3d160",
+    "#606bd3",
+    "#60c2d3"
+  ]; 
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
+  
+
+  pads.forEach((pad, index) => {
+    pad.addEventListener("click", function() {
+      sounds[index].currentTime = 0;
+      sounds[index].play();
+      createBubble(index);
+    });
+  });
+
+  const createBubble = index => {
+    //Create bubbles
+    const bubble = document.createElement("div");
+    visual.appendChild(bubble);
+    bubble.style.backgroundColor = colors[index];
+    bubble.style.animation = `jump 1s ease`;
+    bubble.addEventListener("animationend", function() {
+      visual.removeChild(this);
+    });
+  };
+});
